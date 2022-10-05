c2debfdf0902f79c004ec010378608249ea3a7e0
Hasan <dev.hahuaz@gmail.com>
2022-10-05 12:26:44 +0300

    implement ajax request for getting sentences from backend

--- pages/sentence/[word].tsx
+++ pages/sentence/[word].tsx
@@ -9,35 +9,41 @@ const Home: NextPage = () => {
 
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
   const [clickedId, setClickedId] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [sentences, setSentences] = useState<any>(null);
+
+  const BACKEND_URL =
+    'https://wvqpyejigd.execute-api.us-west-2.amazonaws.com/prod/';
+
+  useEffect(() => {
+    if (!word) return;
+    setIsLoading(true);
+    const controller = new AbortController();
+    try {
+      const getData = async () => {
+        const response = await fetch(`${BACKEND_URL}sentence?word=${word}`, {
+          signal: controller.signal,
+        });
+
+        const sentences = await response.json();
+        console.log(sentences);
+        setSentences(sentences.Items);
+      };
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+    return () => {
+      controller.abort();
+    };
+  }, [word]);
 
   const handlePauseEvent = useCallback(() => {
     console.log('pause handler did run');
     setClickedId('');
   }, []);
-
-  const exampleSentences = [
-    {
-      id: '0',
-      word: 'bet',
-      sentence: 'I bet they were good.',
-      audioUrl:
-        'https://insentences-tryout.s3.us-west-2.amazonaws.com/bet/.b0cbd044-3b07-423a-b870-94410bd0cb87.mp3',
-    },
-    {
-      id: '1',
-      word: 'bet',
-      sentence: 'I bet you had a great time there.',
-      audioUrl:
-        'https://insentences-tryout.s3.us-west-2.amazonaws.com/bet/.ebf1b94b-bc06-4975-97f2-a5131013e5dd.mp3',
-    },
-    {
-      id: '2',
-      word: 'bet',
-      sentence: 'Look at this place, I bet they have something nice to eat.',
-      audioUrl:
-        'https://insentences-tryout.s3.us-west-2.amazonaws.com/bet/.688fe955-d8fc-4ca4-9061-978063febd38.mp3',
-    },
-  ];
 
   useEffect(() => {
     if (!clickedId) return;
@@ -47,9 +53,7 @@ const Home: NextPage = () => {
     audio ? audio.pause() : '';
     setAudio(null);
 
-    const sentence = exampleSentences.find(
-      (sentence) => clickedId === sentence.id
-    );
+    const sentence = sentences.find((sentence) => clickedId === sentence.id);
     const audioElement = new Audio(sentence.audioUrl);
     audioElement.addEventListener('pause', handlePauseEvent);
     setAudio(audioElement);
@@ -75,21 +79,23 @@ const Home: NextPage = () => {
       <main className="">
         <div className="">
           <ul className="list-decimal">
-            {exampleSentences.map((example, _i) => {
-              return (
-                <li key={example.id}>
-                  {example.sentence}
-                  <button
-                    className={`${clickedId === example.id ? 'play' : ''}`}
-                    onClick={() => handlePlayClick(example.id)}
-                  >
-                    <span className="material-symbols-outlined icon-volume-up text-gray-500 ">
-                      volume_up
-                    </span>
-                  </button>
-                </li>
-              );
-            })}
+            {sentences
+              ? sentences.map((sentence, _i) => {
+                  return (
+                    <li key={sentence.id}>
+                      {sentence.sentence}
+                      <button
+                        className={`${clickedId === sentence.id ? 'play' : ''}`}
+                        onClick={() => handlePlayClick(sentence.id)}
+                      >
+                        <span className="material-symbols-outlined icon-volume-up text-gray-500 ">
+                          volume_up
+                        </span>
+                      </button>
+                    </li>
+                  );
+                })
+              : 'loading'}
           </ul>
         </div>
       </main>
