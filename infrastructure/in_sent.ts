671fb9766ed5031736e4eb9ef48fe5920a5df13d
Hasan <dev.hahuaz@gmail.com>
2023-07-29 02:26:21 +0300

    turn spa to ssg for better seo

--- infrastructure/in_sent.ts
+++ infrastructure/in_sent.ts
@@ -1,21 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 import {
   aws_cloudfront,
   aws_cloudfront_origins as cfOrigins,
   aws_certificatemanager as cm,
   aws_ssm,
-} from 'aws-cdk-lib';
+} from "aws-cdk-lib";
 
-import { ApiConstruct } from './constructs/api';
-import { LambdaConstruct } from './constructs/lambda';
-import { StorageConstruct } from './constructs/storage';
+import { ApiConstruct } from "./constructs/api";
+import { LambdaConstruct } from "./constructs/lambda";
+import { StorageConstruct } from "./constructs/storage";
 
 export class InSent extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    const BRANCH = this.node.tryGetContext('BRANCH');
+    const BRANCH = this.node.tryGetContext("BRANCH");
     const { DOMAIN_NAME } = this.node.tryGetContext(BRANCH);
 
     const { DOMAIN_CERTIFICATE, STAR_SUBDOMAIN_CERTIFICATE } = process.env;
@@ -80,7 +80,7 @@ export class InSent extends cdk.Stack {
 
     const customCachePolicy = new aws_cloudfront.CachePolicy(
       this,
-      'cachePolicy',
+      "cachePolicy",
       {
         defaultTtl: cdk.Duration.days(0),
         minTtl: cdk.Duration.minutes(0),
@@ -90,7 +90,7 @@ export class InSent extends cdk.Stack {
 
     const customResponsePolicy = new aws_cloudfront.ResponseHeadersPolicy(
       this,
-      'customResponsePolicy2',
+      "customResponsePolicy2",
       {
         // TODO activate browser cache
         // customHeadersBehavior: {
@@ -103,9 +103,9 @@ export class InSent extends cdk.Stack {
         //   ],
         // },
         corsBehavior: {
-          accessControlAllowOrigins: ['*'],
-          accessControlAllowMethods: ['HEAD', 'GET', 'OPTIONS'],
-          accessControlAllowHeaders: ['*'],
+          accessControlAllowOrigins: ["*"],
+          accessControlAllowMethods: ["HEAD", "GET", "OPTIONS"],
+          accessControlAllowHeaders: ["*"],
           originOverride: true,
           accessControlAllowCredentials: false,
         },
@@ -116,7 +116,7 @@ export class InSent extends cdk.Stack {
 
     const siteBucketDist = new aws_cloudfront.Distribution(
       this,
-      'siteBucketDist',
+      "siteBucketDist",
       {
         defaultBehavior: {
           origin: siteBucketOrigin,
@@ -127,7 +127,7 @@ export class InSent extends cdk.Stack {
           responseHeadersPolicy: customResponsePolicy,
         },
         additionalBehaviors: {
-          'letter/*': {
+          "letter/*": {
             origin: siteBucketOrigin,
             edgeLambdas: [
               {
@@ -136,7 +136,7 @@ export class InSent extends cdk.Stack {
               },
             ],
           },
-          'sentence/*': {
+          "sentence/*": {
             origin: siteBucketOrigin,
             edgeLambdas: [
               {
@@ -145,29 +145,29 @@ export class InSent extends cdk.Stack {
               },
             ],
           },
-          'sitemap.xml': {
-            origin: siteBucketOrigin,
-            edgeLambdas: [
-              {
-                functionVersion: sitemapMapper.currentVersion,
-                eventType: aws_cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
-              },
-            ],
-          },
+          // 'sitemap.xml': {
+          //   origin: siteBucketOrigin,
+          //   edgeLambdas: [
+          //     {
+          //       functionVersion: sitemapMapper.currentVersion,
+          //       eventType: aws_cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
+          //     },
+          //   ],
+          // },
         },
         // defaultRootObject: '/index.html',
         errorResponses: [
           {
             httpStatus: 404,
             responseHttpStatus: 404,
-            responsePagePath: '/404.html',
+            responsePagePath: "/404.html",
           },
         ],
         domainNames: [DOMAIN_NAME],
         // CF distribution only accept certificates that is on us-east-1
         certificate: cm.Certificate.fromCertificateArn(
           this,
-          'DOMAIN_CERTIFICATE',
+          "DOMAIN_CERTIFICATE",
           DOMAIN_CERTIFICATE as string
         ),
       }
@@ -175,7 +175,7 @@ export class InSent extends cdk.Stack {
 
     const audioBucketDist = new aws_cloudfront.Distribution(
       this,
-      'audioBucketDist',
+      "audioBucketDist",
       {
         defaultBehavior: {
           origin: new cfOrigins.S3Origin(audioBucket),
@@ -184,7 +184,7 @@ export class InSent extends cdk.Stack {
           allowedMethods: aws_cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
           cachePolicy: new aws_cloudfront.CachePolicy(
             this,
-            'audioBucketDistCachePolicy',
+            "audioBucketDistCachePolicy",
             {
               defaultTtl: cdk.Duration.days(30),
               minTtl: cdk.Duration.minutes(30),
@@ -193,7 +193,7 @@ export class InSent extends cdk.Stack {
           ),
           responseHeadersPolicy: new aws_cloudfront.ResponseHeadersPolicy(
             this,
-            'audioBucketDistResponsePolicy',
+            "audioBucketDistResponsePolicy",
             {
               // TODO activate browser cache
               // customHeadersBehavior: {
@@ -206,9 +206,9 @@ export class InSent extends cdk.Stack {
               //   ],
               // },
               corsBehavior: {
-                accessControlAllowOrigins: ['*'],
-                accessControlAllowMethods: ['HEAD', 'GET', 'OPTIONS'],
-                accessControlAllowHeaders: ['*'],
+                accessControlAllowOrigins: ["*"],
+                accessControlAllowMethods: ["HEAD", "GET", "OPTIONS"],
+                accessControlAllowHeaders: ["*"],
                 originOverride: true,
                 accessControlAllowCredentials: false,
               },
@@ -220,7 +220,7 @@ export class InSent extends cdk.Stack {
         // CF distribution only accept certificates that is on us-east-1
         certificate: cm.Certificate.fromCertificateArn(
           this,
-          'STAR_SUBDOMAIN_CERTIFICATE',
+          "STAR_SUBDOMAIN_CERTIFICATE",
           STAR_SUBDOMAIN_CERTIFICATE as string
         ),
       }
@@ -229,13 +229,13 @@ export class InSent extends cdk.Stack {
     // TODO create hosted zone by cdk
 
     // CFN OUTPUTS
-    new cdk.CfnOutput(this, 'siteBucketDistDomain', {
+    new cdk.CfnOutput(this, "siteBucketDistDomain", {
       value: siteBucketDist.distributionDomainName,
-      description: 'The domain name of siteBucketDist',
+      description: "The domain name of siteBucketDist",
     });
-    new cdk.CfnOutput(this, 'audioBucketDistDomain', {
+    new cdk.CfnOutput(this, "audioBucketDistDomain", {
       value: audioBucketDist.distributionDomainName,
-      description: 'The domain name of audioBucketDist',
+      description: "The domain name of audioBucketDist",
     });
   }
 }
